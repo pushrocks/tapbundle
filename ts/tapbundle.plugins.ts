9892085140d01e60be05fe64e27f3a921fc0eeb5
Phil Kunz <gitlab@philkunz.com>
2019-06-19 13:18:45 +0200

    fix(core): update

--- ts/tapbundle.plugins.ts
+++ ts/tapbundle.plugins.ts
@@ -1,6 +1,14 @@
+// pushrocks
 import * as early from '@pushrocks/early';
-import * as leakage from 'leakage';
 import * as smartdelay from '@pushrocks/smartdelay';
 import * as smartpromise from '@pushrocks/smartpromise';
 
-export { early, smartdelay, smartpromise, leakage };
+export { early, smartdelay, smartpromise };
+
+// third party
+import * as leakage from 'leakage';
+
+export {
+  leakage
+};
+
